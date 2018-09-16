f4e060a5ae0b29f86c0d113405cdf8b1711904a6
GattyGamaun <gattygamaun@gmail.com>
2018-09-16 20:02:04 +0300

    sorter/fix: improve solution

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
 class Sorter {
   constructor() {
     this.storage = [];
-   }
+  }
  
    add(element) {
     this.storage.push(element);
-   }
+  }
 
    at(index) {
      return this.storage[index];
-   }
+  }
 
    get length() {
      return this.storage.length;
-   }
+  }
 
    toArray() {
     return this.storage;
-   }
+  }
 
    sort(indices) {
+    indices.sort((a, b) => a < b ? -1 : (a > b ? 1 : 0)); 
     let length = indices.length;
     for(let j = 0; j < length - 1; j++){
         for (let i = 1; i < length - j; i++){
@@ -31,10 +32,10 @@ class Sorter {
         }
     }
     return  this.storage;
-}
+  }
 
   setComparator(compareFunction) {
-    // your implementation
+    // return b.this.storage - a.this.storage;
   }
 }
 
